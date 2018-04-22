220b4362981787f926334a3dbf170090f8f8cce6
Emre Köse <e.kose26@gmail.com>
2018-04-22 15:17:14 +0300

    recipe steps displayed with material stepper view

--- BakingApp/app/src/main/java/com/emrekose/bakingapp/ui/detail/RecipeDetailFragment.java
+++ BakingApp/app/src/main/java/com/emrekose/bakingapp/ui/detail/RecipeDetailFragment.java
@@ -1,12 +1,16 @@
 package com.emrekose.bakingapp.ui.detail;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.emrekose.bakingapp.R;
@@ -15,15 +19,22 @@ import com.emrekose.bakingapp.utils.Constants;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import moe.feng.common.stepperview.IStepperAdapter;
+import moe.feng.common.stepperview.VerticalStepperItemView;
+import moe.feng.common.stepperview.VerticalStepperView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RecipeDetailFragment extends Fragment {
+public class RecipeDetailFragment extends Fragment implements IStepperAdapter {
 
     @BindView(R.id.recipe_details_ingredients)
     TextView recipeIngredients;
 
+    @BindView(R.id.recipe_steps)
+    VerticalStepperView verticalStepperView;
+
+    RecipeResponse response;
 
     public RecipeDetailFragment() {
         // Required empty public constructor
@@ -48,12 +59,18 @@ public class RecipeDetailFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
         if (getActivity().getIntent() != null) {
-            RecipeResponse response = (RecipeResponse) getActivity().getIntent().getExtras().getSerializable(Constants.RECIPES_EXTRA);
+            response = (RecipeResponse) getActivity().getIntent().getExtras().getSerializable(Constants.RECIPES_EXTRA);
             setIngredients(response);
+            verticalStepperView.setStepperAdapter(this);
         }
     }
 
@@ -69,4 +86,64 @@ public class RecipeDetailFragment extends Fragment {
         }
     }
 
+    @NonNull
+    @Override
+    public CharSequence getTitle(int i) {
+        return "Step " + (i + 1);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getSummary(int i) {
+        if (i == 0) {
+            return Html.fromHtml(String.format("First step", response.getName()));
+        } else {
+            if (i != size() - 1) {
+                return Html.fromHtml(response.getSteps().get(i - 1).getDescription());
+            } else {
+                return Html.fromHtml(String.format("Last step", response.getName()));
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        return response.getIngredients().size();
+    }
+
+    @Override
+    public View onCreateCustomView(int index, Context context, VerticalStepperItemView parent) {
+        View inflateView = LayoutInflater.from(context).inflate(R.layout.vertical_stepper_item, parent, false);
+
+        TextView contentView = inflateView.findViewById(R.id.item_content);
+        contentView.setText(response.getSteps().get(index).getDescription());
+
+        Button nextButton = inflateView.findViewById(R.id.button_next);
+        nextButton.setOnClickListener(v -> {
+            if (verticalStepperView.canNext()) {
+                verticalStepperView.nextStep();
+            }
+        });
+
+        Button prevButton = inflateView.findViewById(R.id.button_prev);
+        prevButton.setOnClickListener(v -> {
+            if (index != 0) {
+                verticalStepperView.prevStep();
+            } else {
+                verticalStepperView.setAnimationEnabled(!verticalStepperView.isAnimationEnabled());
+            }
+        });
+
+        return inflateView;
+    }
+
+    @Override
+    public void onShow(int i) {
+
+    }
+
+    @Override
+    public void onHide(int i) {
+
+    }
 }
