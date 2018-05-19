9df29ae4b659448f0ef5e406b9bb4d4923da088b
Emre Köse <e.kose26@gmail.com>
2018-05-19 18:26:38 +0300

    Udacity reviewer suggestions added

--- BakingApp/app/src/main/java/com/emrekose/bakingapp/utils/Constants.java
+++ BakingApp/app/src/main/java/com/emrekose/bakingapp/utils/Constants.java
@@ -1,6 +1,6 @@
 package com.emrekose.bakingapp.utils;
 
-public class Constants {
+public final class Constants {
 
     public static final String API_URL = "https://d17h27t6h515a5.cloudfront.net/";
 
