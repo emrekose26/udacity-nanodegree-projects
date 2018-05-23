86f1d7c0f3bf1d69e35a28ff3e9dd5cfd19c3041
Emre Köse <e.kose26@gmail.com>
2018-05-23 13:58:09 +0300

    api endpoint added to backend module

--- BuildItBigger/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ BuildItBigger/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,10 +1,9 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.emrekose.jokeprovider.Jokes;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
-import javax.inject.Named;
 
 /** An endpoint class we are exposing */
 @Api(
@@ -18,12 +17,11 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
+        final String joke = Jokes.getRandomJoke();
+        response.setData(joke);
         return response;
     }
 
