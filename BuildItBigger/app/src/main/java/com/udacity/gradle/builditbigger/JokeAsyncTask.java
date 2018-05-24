2050f437d8a4c215d159230dd74e79b6fb1795c1
Emre Köse <e.kose26@gmail.com>
2018-05-24 14:58:41 +0300

    JokeAsyncTask added

---
+++ BuildItBigger/app/src/main/java/com/udacity/gradle/builditbigger/JokeAsyncTask.java
@@ -0,0 +1,56 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import com.emrekose.jokedisplayer.JokeDisplayActivity;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
+
+public class JokeAsyncTask extends AsyncTask<Void, Void, String> {
+
+    private static MyApi myApi = null;
+    private Context context;
+
+    public JokeAsyncTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        if (myApi == null) { // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
+                            request.setDisableGZipContent(true);
+                        }
+                    });
+            myApi = builder.build();
+        }
+
+        try {
+            return myApi.getJoke().execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Intent intent = new Intent(context, JokeDisplayActivity.class);
+        intent.putExtra("joke", result);
+        context.startActivity(intent);
+    }
+}
