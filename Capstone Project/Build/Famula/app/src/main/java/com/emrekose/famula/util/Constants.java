18be8034495cc33ef17db598b8ff10254d609acc
Emre Köse <e.kose26@gmail.com>
2018-06-28 15:36:28 +0300

    Zomato API KEY obtained

---
+++ Capstone Project/Build/Famula/app/src/main/java/com/emrekose/famula/util/Constants.java
@@ -0,0 +1,7 @@
+package com.emrekose.famula.util;
+
+import com.emrekose.famula.BuildConfig;
+
+public final class Constants {
+    public static final String API_KEY = BuildConfig.ZOMATO_API_KEY;
+}
