337facd0ade7cc7bce4272ecdd1f629d3cb90725
Emre Köse <e.kose26@gmail.com>
2018-06-28 14:45:29 +0300

    BaseActivity and BaseFragment classes added

---
+++ Capstone Project/Build/Famula/app/src/main/java/com/emrekose/famula/common/BaseActivity.java
@@ -0,0 +1,39 @@
+package com.emrekose.famula.common;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+
+public abstract class BaseActivity<DB extends ViewDataBinding> extends AppCompatActivity implements HasSupportFragmentInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Fragment> fragmentDispatchingAndroidInjector;
+
+    public DB dataBinding;
+
+    @LayoutRes
+    public abstract int getLayoutRes();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+        super.onCreate(savedInstanceState);
+        dataBinding = DataBindingUtil.setContentView(this, getLayoutRes());
+    }
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return fragmentDispatchingAndroidInjector;
+    }
+}
---
+++ Capstone Project/Build/Famula/app/src/main/java/com/emrekose/famula/common/BaseFragment.java
@@ -0,0 +1,48 @@
+package com.emrekose.famula.common;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import javax.inject.Inject;
+
+import dagger.android.support.AndroidSupportInjection;
+
+public abstract class BaseFragment<VM extends ViewModel, DB extends ViewDataBinding> extends Fragment {
+
+    @Inject
+    ViewModelProvider.Factory viewModelFactory;
+
+    public VM viewModel;
+
+    public DB dataBinding;
+
+    public abstract Class<VM> getViewModel();
+
+    @LayoutRes
+    public abstract int getLayoutRes();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        AndroidSupportInjection.inject(this);
+        super.onCreate(savedInstanceState);
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(getViewModel());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        dataBinding = DataBindingUtil.inflate(inflater, getLayoutRes(), container, false);
+        return dataBinding.getRoot();
+    }
+}
