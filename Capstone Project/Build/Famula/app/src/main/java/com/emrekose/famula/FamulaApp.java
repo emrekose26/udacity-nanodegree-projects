5d7dfdb5a75e04d1d9c7bf3c1e681a4a642b96de
Emre Köse <e.kose26@gmail.com>
2018-06-30 15:20:36 +0300

    Stetho and Timber initialization

--- Capstone Project/Build/Famula/app/src/main/java/com/emrekose/famula/FamulaApp.java
+++ Capstone Project/Build/Famula/app/src/main/java/com/emrekose/famula/FamulaApp.java
@@ -1,11 +1,23 @@
 package com.emrekose.famula;
 
 import com.emrekose.famula.di.DaggerAppComponent;
+import com.facebook.stetho.Stetho;
 
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
+import timber.log.Timber;
 
 public class FamulaApp extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this);
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
 
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
