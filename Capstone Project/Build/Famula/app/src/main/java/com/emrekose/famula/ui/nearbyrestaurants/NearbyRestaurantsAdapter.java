4e64002a450de9f624c28c2c01eb73dd853a763e
Emre Köse <e.kose26@gmail.com>
2018-07-04 15:51:48 +0300

    Nearby restaurants adapter and callback created

---
+++ Capstone Project/Build/Famula/app/src/main/java/com/emrekose/famula/ui/nearbyrestaurants/NearbyRestaurantsAdapter.java
@@ -0,0 +1,67 @@
+package com.emrekose.famula.ui.nearbyrestaurants;
+
+import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.emrekose.famula.databinding.NearbyRestaurantsItemBinding;
+import com.emrekose.famula.model.geocode.NearbyRestaurant;
+
+public class NearbyRestaurantsAdapter extends ListAdapter<NearbyRestaurant, NearbyRestaurantsAdapter.ViewHolder> {
+
+    private NearbyRestaurantsCallback callback;
+
+    protected NearbyRestaurantsAdapter(@NonNull DiffUtil.ItemCallback<NearbyRestaurant> diffCallback, NearbyRestaurantsCallback callback) {
+        super(diffCallback);
+        this.callback = callback;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return ViewHolder.create(LayoutInflater.from(parent.getContext()), parent, callback);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(getItem(position));
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        NearbyRestaurantsItemBinding binding;
+
+        public ViewHolder(NearbyRestaurantsItemBinding binding, NearbyRestaurantsCallback callback) {
+            super(binding.getRoot());
+            this.binding = binding;
+            binding.getRoot().setOnClickListener(v ->
+                callback.onNearbyRestaurantClick(binding.getRestaurant()));
+        }
+
+        public static ViewHolder create(LayoutInflater inflater, ViewGroup parent, NearbyRestaurantsCallback callback) {
+            NearbyRestaurantsItemBinding nearbyRestaurantsItemBinding = NearbyRestaurantsItemBinding.inflate(inflater, parent, false);
+            return new ViewHolder(nearbyRestaurantsItemBinding, callback);
+        }
+
+        public void bind(NearbyRestaurant restaurant) {
+            binding.setRestaurant(restaurant);
+            binding.executePendingBindings();
+        }
+    }
+
+    static class NearbyRestaurantsDiffCallback extends DiffUtil.ItemCallback<NearbyRestaurant> {
+
+        @Override
+        public boolean areItemsTheSame(NearbyRestaurant oldItem, NearbyRestaurant newItem) {
+            return oldItem.getRestaurant().getId().equals(newItem.getRestaurant().getId());
+        }
+
+        @Override
+        public boolean areContentsTheSame(NearbyRestaurant oldItem, NearbyRestaurant newItem) {
+            return oldItem.getRestaurant().getName().equals(newItem.getRestaurant().getName());
+        }
+    }
+}
---
+++ Capstone Project/Build/Famula/app/src/main/java/com/emrekose/famula/ui/nearbyrestaurants/NearbyRestaurantsCallback.java
@@ -0,0 +1,7 @@
+package com.emrekose.famula.ui.nearbyrestaurants;
+
+import com.emrekose.famula.model.geocode.NearbyRestaurant;
+
+public interface NearbyRestaurantsCallback {
+    void onNearbyRestaurantClick(NearbyRestaurant restaurant);
+}
