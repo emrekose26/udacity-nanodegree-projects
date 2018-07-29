01810cecd074fd40842be2c5fb17b89bf177391a
Emre Köse <e.kose26@gmail.com>
2018-07-29 12:47:50 +0300

    nearby restaurant some files repacked

---
+++ Capstone Project/Build/Famula/app/src/main/java/com/emrekose/famula/ui/nearbyrestaurants/NearbyRestaurantsMainCallback.java
@@ -0,0 +1,7 @@
+package com.emrekose.famula.ui.nearbyrestaurants;
+
+import com.emrekose.famula.model.geocode.NearbyRestaurant;
+
+public interface NearbyRestaurantsMainCallback {
+    void onMainNearbyRestaurantsClick(NearbyRestaurant nearbyRestaurant);
+}
